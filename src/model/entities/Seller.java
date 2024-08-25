70923425582c8d687f0caf463a470917a81b7a3f
vitorHugo87 <vitorpaduan87@gmail.com>
2024-08-25 20:04:57 -0300

    update Implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -2,7 +2,7 @@ package model.entities;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.Objects;
+
 
 public class Seller implements Serializable{
 
@@ -78,7 +78,10 @@ public class Seller implements Serializable{
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -90,7 +93,12 @@ public class Seller implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Seller other = (Seller) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 	@Override
